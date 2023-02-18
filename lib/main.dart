c345f8ab6b45e3d1b49e700ec89a276a5b3fbd54
Pham Ngoc Duong <pnd280@gmail.com>
2023-02-18 19:48:46 +0700

    add floating action btn

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:taskly/pages/calendar_view.dart';
 import 'package:taskly/pages/settings.dart';
@@ -17,6 +18,7 @@ class MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'Inter',
+        primarySwatch: Colors.purple,
       ),
       home: const RootPage(),
     );
@@ -33,7 +35,7 @@ class RootPage extends StatefulWidget {
 class RootPageState extends State<RootPage> {
   int currentNavPage = 0;
 
-  List<Widget> pages = [
+  List<Widget> pages = const [
     TaskOverallViewPage(),
     CalendarViewPage(),
     SettingsPage(),
@@ -43,7 +45,18 @@ class RootPageState extends State<RootPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: pages[currentNavPage],
-      bottomNavigationBar: Container(
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: currentNavPage != 2
+          ? FloatingActionButton(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(11)),
+              onPressed: () {},
+              child: const Icon(
+                CupertinoIcons.add,
+                size: 30,
+              ),
+            )
+          : Container(),
+      bottomNavigationBar: SizedBox(
         height: 80,
         child: BottomNavigationBar(
           currentIndex: currentNavPage,
@@ -52,8 +65,10 @@ class RootPageState extends State<RootPage> {
           ),
           items: const [
             BottomNavigationBarItem(icon: Icon(Icons.task_alt), label: 'Task'),
-            BottomNavigationBarItem(icon: Icon(Icons.calendar_month_outlined), label: 'Calendar'),
-            BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.calendar_month_outlined), label: 'Calendar'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: 'Settings'),
           ],
           onTap: (int index) {
             setState(() {
