0718fdf7704f4773cfcd9545ce096e3936736f83
Pham Ngoc Duong <pnd280@gmail.com>
2023-02-18 19:31:39 +0700

    add Navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:taskly/pages/calendar_view.dart';
+import 'package:taskly/pages/settings.dart';
+import 'package:taskly/pages/task_overall_view.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +12,39 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Taskly'),
+    return MaterialApp(
+      title: 'Taskly',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Inter',
+      ),
+      home: const RootPage(),
+    );
+  }
+}
+
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => RootPageState();
+}
+
+class RootPageState extends State<RootPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: Container(
+        height: 80,
+        child: BottomNavigationBar(
+          selectedLabelStyle: const TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.task_alt), label: 'Task'),
+            BottomNavigationBarItem(icon: Icon(Icons.calendar_month_outlined), label: 'Calendar'),
+            BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
+          ],
         ),
       ),
     );
