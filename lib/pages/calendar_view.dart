6ee59989b05a5340675ed3196ad5263aecfa660b
Pham Ngoc Duong <pnd280@gmail.com>
2023-02-19 16:30:35 +0700

    tweak calendar view

--- lib/pages/calendar_view.dart
+++ lib/pages/calendar_view.dart
@@ -1,5 +1,13 @@
+import 'dart:math';
+
 import 'package:calendar_view/calendar_view.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:taskly/miscs/colors.dart';
+import 'package:taskly/miscs/utils.dart';
+
+GlobalKey<MonthViewState> _calendarMonthViewKey = GlobalKey<MonthViewState>();
+GlobalKey<DayViewState> _calendarDayViewKey = GlobalKey<DayViewState>();
 
 class CalendarViewPage extends StatefulWidget {
   int view;
@@ -13,10 +21,50 @@ class CalendarViewPage extends StatefulWidget {
 class _CalendarViewPageState extends State<CalendarViewPage> {
   @override
   Widget build(BuildContext context) {
-    debugPrint(widget.view.toString());
     return Scaffold(
       backgroundColor: Colors.white,
-      body: (widget.view == 0) ? MonthView() : DayView(),
+      body: CalendarView(widget.view),
     );
+  }
+
+  // widget CalendarView returns a widget that displays a calendar (month, day)
+  Widget CalendarView(int view) {
+    switch (view) {
+      case 0:
+        return MonthView(
+          key: _calendarMonthViewKey,
+          showBorder: false,
+          weekDayBuilder: (day) {
+            return Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: Container(
+                height: 30,
+                decoration: const BoxDecoration(
+                    // color: TasklyColor.VeriPeri,
+                    ),
+                child: Center(
+                  child: Text(
+                    intToWeekDay(day),
+                    style: const TextStyle(
+                      color: TasklyColor.greyText,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+          onCellTap: (events, date) {
+            
+          },
+        );
+      case 1:
+        return DayView(
+          key: _calendarDayViewKey,
+        );
+      default:
+        return const MonthView();
+    }
   }
 }
